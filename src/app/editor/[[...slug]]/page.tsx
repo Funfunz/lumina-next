365906bf7742f26c999b77eccab798c381d0e998
Bruno <bruno.mirrado@merkle.com>
2024-02-14 15:57:26 +0000

    added new css structure

--- src/app/editor/[[...slug]]/page.tsx
+++ src/app/editor/[[...slug]]/page.tsx
@@ -1,23 +1,32 @@
-import { Editor } from "@/components/editor/editor"
-import { getFullData } from "@/lib/dataFetcher"
-import { Render } from "@/components/render/render"
-import { ContextProvider } from '@/context/contextProvider'
+import { Editor } from "@/components/editor/editor";
+import { getFullData } from "@/lib/dataFetcher";
+import { Render } from "@/components/render/render";
+import { ContextProvider } from "@/context/contextProvider";
 
 type Props = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
-export default async function EditorPage({params}: Props) {
-  const selectedPage = params.slug || 'home'
-  const builderData = await getFullData()
-  if (!builderData[selectedPage]) return
+export default async function EditorPage({ params }: Props) {
+  const selectedPage = params.slug || "home";
+  const builderData = await getFullData();
+  if (!builderData[selectedPage]) return;
   return (
-    <ContextProvider data={{appContext: {editor: true}, builderDataContext: {builderData, selectedPage, pages: Object.keys(builderData)}}}>
+    <ContextProvider
+      data={{
+        appContext: { editor: true },
+        builderDataContext: {
+          builderData,
+          selectedPage,
+          pages: Object.keys(builderData),
+        },
+      }}
+    >
       <Editor>
-        <Render/>
+        <Render />
       </Editor>
     </ContextProvider>
-  )
+  );
 }
