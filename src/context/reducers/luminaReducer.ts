0edb08764ff4ea8ff463fa1a21d2f82e8761422f
Joao Moura <joao.moura@merkle.com>
2024-02-16 10:00:10 +0000

    Changed reducers folder structure

--- src/context/reducers/luminaReducer.ts
+++ src/context/reducers/luminaReducer.ts
@@ -1,7 +1,11 @@
 'use client'
- 
+
+/**
+ * Lumina main reducer
+ * It turns all the reducers into a single one
+ */
 import { Dispatch } from 'react'
-import { IAppContext, TAppContextAction, contextReducer, initialAppContextState } from './appContext'
+import { IAppContext, TAppContextAction, appContextReducer, initialAppContextState } from './appContext'
 import { IBuilderDataContext, builderDataContextReducer, TBuilderDataContextAction, initialBuilderDataContextState } from './builderDataContext'
 
 export type TAppContextDispatch = Dispatch<TAppContextAction | TBuilderDataContextAction> 
@@ -23,7 +27,7 @@ export const mainReducer = (
   }: IInitialStateType,
   action: TAppContextAction | TBuilderDataContextAction
 ) => ({
-  appContext: contextReducer(appContext, action as TAppContextAction),
+  appContext: appContextReducer(appContext, action as TAppContextAction),
   builderDataContext: builderDataContextReducer(builderDataContext, action as TBuilderDataContextAction)
 })
 
