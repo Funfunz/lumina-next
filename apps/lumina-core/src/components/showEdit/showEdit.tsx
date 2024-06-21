bd715a33da730b1148a8df19058f18d1524248b5
Bruno <bruno.mirrado@merkle.com>
2024-06-21 12:59:25 +0100

    fixed the issue with the rendering of the new icons

--- apps/lumina-core/src/components/showEdit/showEdit.tsx
+++ apps/lumina-core/src/components/showEdit/showEdit.tsx
@@ -83,7 +83,7 @@ export const ShowEdit = ({
   let [showModalDelete, setShowModalDelete] = useState(false); //Delete Modal - BM
   let [toDeleteInput, setToDeleteInput] = useState(""); //Delete Modal - Input
   let [deleteInputError, setDeleteInputError] = useState(false); //Delete Modal - error
-  
+
   let [formData, setFormData] = useState(data || {});
   let [selectedOption, setSelectedOption] = useState<{value: string, label: string}>(); //dropdown - new component
   let [newComponentFriendlyName, setNewComponentFriendlyName] = useState("") //friendly name - new component
@@ -296,7 +296,7 @@ export const ShowEdit = ({
               color="secondary"
               outline
               onClick={handleCloseModal}
-              iconRight="lumina-cross"
+              iconRight="lumina-cut"
             />
           </div>
         </ReactModal>
@@ -430,9 +430,9 @@ export const ShowEdit = ({
           ) || null}
         </div>
       )}
-      
 
-      
+
+
     </>
   );
 };
