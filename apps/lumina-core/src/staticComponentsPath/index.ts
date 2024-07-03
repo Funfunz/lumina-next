394b8a7b50bd76e11aa864dc5b8fecc93f593982
Bruno <bruno.mirrado@merkle.com>
2024-07-03 12:48:10 +0100

    fixed another missing import

--- apps/lumina-core/src/staticComponentsPath/index.ts
+++ apps/lumina-core/src/staticComponentsPath/index.ts
@@ -3,15 +3,12 @@ import { Grid } from "@/components/grid/grid"
 import { Image } from "@/components/image/image"
 import { TitleText } from "@/components/titleText/titleText"
 import { Flex } from "@/components/flex/flex"
-
 import { config as configLinkBox } from "@/components/linkBox/config"
 import { config as configGrid } from "@/components/grid/config"
 import { config as configImage } from "@/components/image/config"
 import { config as configTitleText} from "@/components/titleText/config"
 import { config as configFlex } from "@/components/flex/config"
-import { TConfig } from "@/lumina-models/lumina-showEditModel"
-
-
+import { TConfig } from "@/models/showEditModel"
 
 const paths: {
   [key: string]: (data: any) => React.JSX.Element | null
