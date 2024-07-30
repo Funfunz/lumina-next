b967bc070ef4b83a699132f3cb512ac1cc6c062a
adriana.alves <adriana.alves@merkle.com>
2024-07-30 21:14:47 +0100

    Hover effect on buttons -sidebar exception not working

--- packages/lumina-core/src/components/button/button.tsx
+++ packages/lumina-core/src/components/button/button.tsx
@@ -1,7 +1,6 @@
-
-import { ButtonContent } from "./buttonContent/buttonContent"
-import cx from "classnames"
-import { TLumButton } from "./button-models"
+import { ButtonContent } from "./buttonContent/buttonContent";
+import cx from "classnames";
+import { TLumButton } from "./button-models";
 
 /**
  *
@@ -20,46 +19,34 @@ import { TLumButton } from "./button-models"
  */
 
 export const Button = (props: TLumButton) => {
-  const { className, style, buttonType, text, iconLeft, iconRight, size } = props
-  const allClassNames = `${style ? style : ''} ${className ? className : ''} ${size ? size : '' }`
+  const { className, style, buttonType, text, iconLeft, iconRight, size } =
+    props;
+  const allClassNames = `${style ? style : ""} ${className ? className : ""} ${size ? size : ""}`;
 
   if (buttonType === "button") {
-    const { onClick, disabled, isFullWidth } = props
+    const { onClick, disabled, isFullWidth } = props;
     return (
       <button
-        className={cx('button', allClassNames, { fullWidth: isFullWidth })}
+        className={cx("button", allClassNames, { fullWidth: isFullWidth })}
         onClick={onClick}
         disabled={disabled}
       >
-        <ButtonContent
-          text={text}
-          iconLeft={iconLeft}
-          iconRight={iconRight}
-        />
+        <ButtonContent text={text} iconLeft={iconLeft} iconRight={iconRight} />
       </button>
-    )
+    );
   } else if (buttonType === "externalLink") {
-    const { href, target } = props
+    const { href, target } = props;
     return (
       <a className={allClassNames} href={href} target={target}>
-        <ButtonContent
-          text={text}
-          iconLeft={iconLeft}
-          iconRight={iconRight}
-        />
+        <ButtonContent text={text} iconLeft={iconLeft} iconRight={iconRight} />
       </a>
-    )
+    );
   } else if (buttonType === "link") {
-    const { href } = props
+    const { href } = props;
     return (
       <a className={allClassNames} href={href}>
-        <ButtonContent
-          text={text}
-          iconLeft={iconLeft}
-          iconRight={iconRight}
-        />
+        <ButtonContent text={text} iconLeft={iconLeft} iconRight={iconRight} />
       </a>
-    )
+    );
   }
-}
-
+};
