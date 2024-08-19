4d4c3758dc68af5de507756ec2595798b6dbac0d
Bruno <bruno.mirrado@merkle.com>
2024-08-19 14:18:05 +0100

    small optimization on the route tag, replacing it by a self-closing one

--- packages/lumina-core/src/main.tsx
+++ packages/lumina-core/src/main.tsx
@@ -86,7 +86,7 @@ export default function Lumina({ selectedPage, getData, components }: TProps = d
                     <Render />
                   </Editor>
                 }
-              ></Route>
+              />
             </Routes>
           </BrowserRouter>
         </ToggleModalContextProvider>
