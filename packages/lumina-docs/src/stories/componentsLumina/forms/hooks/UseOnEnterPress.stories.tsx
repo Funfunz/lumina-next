6a96fbf9ef4f97a7d51cf82e57f32fbd90a41b2e
Bruno <bruno.mirrado@merkle.com>
2024-09-03 16:56:40 +0100

    feature(form-cleanup): update package.json on lumina-docs

--- packages/lumina-docs/src/stories/componentsLumina/forms/hooks/UseOnEnterPress.stories.tsx
+++ packages/lumina-docs/src/stories/componentsLumina/forms/hooks/UseOnEnterPress.stories.tsx
@@ -1,28 +1,66 @@
-import type { Meta, StoryObj } from '@storybook/react/'
+// import type { Meta, StoryObj } from '@storybook/react/'
 
+// import { useActivateOnEnter } from '@lumina/core/src/components/form-components/utils/hooks/useActivateOnEnter/index.tsx'
+// // This could be imported from your own or external component library
+
+// // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// const meta = {
+//   title: 'Components Lumina/Forms/Hooks/useOnEnterPress',
+//   parameters: {
+//     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+//     layout: 'centered',
+//   },
+//   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+//   tags: ['autodocs'],
+//   // More on argTypes: https://storybook.js.org/docs/api/argtypes
+//   argTypes: {},
+//   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+//   args: {},
+// } satisfies Meta<typeof useActivateOnEnter>
+
+// export default meta
+// type Story = StoryObj<typeof meta>
+
+// // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// export const Primary: Story = {
+//   args: {},
+// }
+
+import React from 'react'
+import { Meta, StoryFn } from '@storybook/react'
 import { useActivateOnEnter } from '@lumina/core/src/components/form-components/utils/hooks/useActivateOnEnter/index.tsx'
-// This could be imported from your own or external component library
+import { Input } from '@lumina/core/src/components/form-components/input/index.js'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Components Lumina/Forms/Hooks/useOnEnterPress',
-  component: useActivateOnEnter,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: {},
-} satisfies Meta<typeof useActivateOnEnter>
+export default {
+  title: 'Components Lumina/Forms/Components/Hooks/useActivateOnEnter',
+  component: () => <div />, // Placeholder component
+} as Meta
 
-export default meta
-type Story = StoryObj<typeof meta>
+// eslint-disable-next-line no-unused-vars
+const Template: StoryFn = (args: {
+  activateEnterPress?: boolean
+  clearOnEnterPress?: boolean
+  onEnterPress?: () => void
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+}) => {
+  const { value, setValue, inputRef, handleKeyDown } = useActivateOnEnter(args)
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {},
+  return (
+    <Input
+      ref={inputRef}
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      onKeyDown={handleKeyDown}
+    />
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  activateEnterPress: true,
+  clearOnEnterPress: false,
+  onEnterPress: () => console.log('Enter pressed'),
+  onChange: (e: { target: { value: any } }) => console.log(e.target.value),
+  value: '',
 }
