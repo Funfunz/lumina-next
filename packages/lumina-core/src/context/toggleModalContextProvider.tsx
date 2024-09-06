220f85293c37f26b0069982c904a1b46f0e60bd1
Bruno <bruno.mirrado@merkle.com>
2024-09-06 10:54:52 +0100

    feat(page-crud): reverted  back to what toggleModalContextProvider was

--- packages/lumina-core/src/context/toggleModalContextProvider.tsx
+++ packages/lumina-core/src/context/toggleModalContextProvider.tsx
@@ -1,35 +1,23 @@
 /* eslint-disable no-unused-vars */
 import { ReactNode, createContext, useContext, useState } from 'react'
-import type { IComponentProps, IPageProps } from '../models/data'
+import type { IComponentProps } from '../models/data'
 import type { TConfig } from '@/models/editor-buttonModel'
 
 export const ADDMODAL = 'ADD'
 export const EDITMODAL = 'EDIT'
 export const DELETEMODAL = 'DELETE'
 
-type TModalComponentProps = TModalBaseProps & {
+type TToggleModalProps = {
   componentProps?: IComponentProps
-  onUpdate?: (data: IComponentProps) => void
-}
-
-type TModalState = TModalComponentProps &
-  TModalPageProps &
-  TModalBaseProps & {
-    isOpen: boolean
-  }
-
-type TModalBaseProps = {
   config?: TConfig
+  onUpdate?: (data: IComponentProps) => void
   id?: string
   modalType: typeof ADDMODAL | typeof EDITMODAL | typeof DELETEMODAL | null
 }
 
-type TModalPageProps = TModalBaseProps & {
-  pageProps?: IPageProps
-  onUpdate?: (data: IPageProps) => void
+type TModalState = TToggleModalProps & {
+  isOpen: boolean
 }
-
-type TToggleModalProps = TModalComponentProps | TModalPageProps
 
 const initialModalState: TModalState = {
   isOpen: false,
@@ -48,7 +36,7 @@ const initialModalState: TModalState = {
 }
 
 type TToggleModalContext = {
-  handleOpenModal: (props: TToggleModalProps) => void
+  handleOpenModal: ({ componentProps, config, id, modalType }: TToggleModalProps) => void
   handleCloseModal: () => void
   modalState: TModalState
 }
@@ -59,35 +47,17 @@ const ToggleModalContext = createContext<TToggleModalContext>({
   modalState: initialModalState,
 })
 
-function isModalComponentProps(props: TToggleModalProps): props is TModalComponentProps {
-  return !!(props as TModalComponentProps).componentProps
-}
-
-function isModalPageProps(props: TToggleModalProps): props is TModalPageProps {
-  return !!(props as TModalPageProps).pageProps
-}
-
 export const ToggleModalContextProvider = ({ children }: { children: ReactNode }) => {
   const [modalState, setModalState] = useState<TModalState>(initialModalState)
 
-  const handleOpenModal = (props: TToggleModalProps) => {
-    if (isModalComponentProps(props)) {
-      setModalState({
-        componentProps: props.componentProps,
-        config: props.config,
-        id: props.id,
-        isOpen: true,
-        modalType: props.modalType,
-      })
-    } else if (isModalPageProps(props)) {
-      setModalState({
-        pageProps: props.pageProps,
-        config: props.config,
-        id: props.id,
-        isOpen: true,
-        modalType: props.modalType,
-      })
-    }
+  const handleOpenModal = ({ id, config, componentProps, modalType }: TToggleModalProps) => {
+    setModalState({
+      componentProps: componentProps,
+      config: config,
+      id: id,
+      isOpen: true,
+      modalType: modalType,
+    })
   }
 
   const handleCloseModal = () => {
