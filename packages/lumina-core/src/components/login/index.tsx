01e3eb77eabc8c0cbc5b60a68d644735cc4084d6
Adriana Alves <adrianaalves098@gmail.com>
2024-09-19 18:33:45 +0100

    Change to modal, not rendering

--- packages/lumina-core/src/components/login/index.tsx
+++ packages/lumina-core/src/components/login/index.tsx
@@ -1,12 +1,16 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState } from 'react'
 import logo_md from '../../assets/logo_md.svg'
+import { Input } from '../form-components/input'
+import { Button } from '../button'
+import { Modal } from '../modals/utils/modal'
+import { Form } from '../editor-buttons-container/inputRenderer'
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState<string>('admin')
   const [password, setPassword] = useState<string>('password')
   const [error, setError] = useState<string>('')
 
-  const handleLogin = (event: FormEvent<HTMLFormElement>) => {
+  const handleLogin = (event: any) => {
     event.preventDefault()
 
     if (username === 'admin' && password === 'password') {
@@ -27,59 +31,82 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div className='lumina_loginPage'>
-      <div className='lumina_loginContainer'>
-        {/* title component */}
-        <h2 className='lumina_loginTitle'>Sign in to Lumina</h2>
-        {/* form component */}
-        <form onSubmit={handleLogin}>
-          <div>
-            <div className='lumina_fieldContainer'>
-              <label htmlFor='username' className='lumina_loginLabel'>
-                Username
-              </label>
-              <p className='lumina_helpText'>Enter your username</p>
-            </div>
-            {/* input component */}
-            <input
-              className='lumina_loginInput'
-              type='text'
-              id='username'
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <div className='lumina_fieldContainer'>
-              <label className='lumina_loginLabel' htmlFor='password'>
-                Password
-              </label>
-              <p className='lumina_helpText'>Enter your password</p>
-            </div>
-            <input
-              className='lumina_loginInput'
-              type='password'
-              id='password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button className='lumina_loginButton' type='submit'>
-            Sign in
-          </button>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-        </form>
-        <button className='lumina_loginLinks' onClick={navigateToRecoverAccount}>
-          Recover account
-        </button>
-        <button className='lumina_loginLinks' onClick={navigateToCreateAccount}>
-          Create account
-        </button>
-        <img src={logo_md} alt='Lumina Logo' />
-      </div>
-    </div>
+    // <div className='lumina_loginPage'>
+    //   <div className='lumina_loginContainer'>
+    <Modal
+      title='Login Page'
+      titleIcon='lum-icon-component'
+      contentLabel='Sign in to Luminasss'
+      content={
+        <div>
+          <Form>
+            <form onSubmit={handleLogin}>
+              <div>
+                <div className='lumina_fieldContainer'>
+                  <label htmlFor='username' className='lumina_loginLabel'>
+                    Username
+                  </label>
+                  <p className='lumina_helpText'>Enter your username</p>
+                </div>
+                {/* input component */}
+                <Input
+                  className='lumina_loginInput'
+                  type='text'
+                  id='username'
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <div className='lumina_fieldContainer'>
+                  <label className='lumina_loginLabel' htmlFor='password'>
+                    Password
+                  </label>
+                  <p className='lumina_helpText'>Enter your password</p>
+                </div>
+                <Input
+                  className='lumina_loginInput'
+                  type='password'
+                  id='password'
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+            </form>
+          </Form>
+          <Button
+            style='lumina_loginLinks'
+            onClick={navigateToRecoverAccount}
+            text='Recover account'
+            buttonType='link'
+            href='./recoverAccount/index.tsx'
+          />
+          <Button
+            style='lumina_loginLinks'
+            onClick={navigateToCreateAccount}
+            text='Create account'
+            buttonType='link'
+            href='./createAccount/index.tsx'
+          />
+          <img src={logo_md} alt='Lumina Logo' />
+        </div>
+      }
+      actions={
+        <Button
+          style='lumina_loginButton'
+          buttonType='button'
+          text=' Sign in'
+          onClick={handleLogin}
+        />
+      }
+    />
+
+    //   </div>
+    // </div>
   )
 }
 
