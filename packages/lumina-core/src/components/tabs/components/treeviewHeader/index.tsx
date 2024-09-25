105970bbe2abb075b8012d9ed5e998d2133a1d16
Adriana Alves <adrianaalves098@gmail.com>
2024-09-25 19:35:16 +0100

    dynamic title

--- packages/lumina-core/src/components/tabs/components/treeviewHeader/index.tsx
+++ packages/lumina-core/src/components/tabs/components/treeviewHeader/index.tsx
@@ -1,16 +1,20 @@
 import { Button } from '@/components/button'
 import cx from 'classnames'
+// import data from '../../../../../../../examples/vite-react/src/data/pageData'
 import { useCallback } from 'react'
 
 export const TreeviewHeader = () => {
   const handleOnClickLive = useCallback(() => {
     window.open(window.location.href.split('/').slice(0, -1).join('/'), '_blank')
   }, [])
+
+  // const friendlyName = data.page1.friendlyName
+
   return (
     <div className='treeviewHeaderContainer'>
       <span className={cx('treeviewHeaderIcon', 'lum-icon-page')}></span>
-      <p className='treeviewTitle'>Home</p>
-      <p className='treeviewSubTitle'>Home Page for Lumina PageBuilder</p>
+      <p className='treeviewTitle'>FriendlyName</p>
+      {/* <p className='treeviewSubTitle'>Home Page for Lumina PageBuilder</p> */}
       <div className='treeviewNavIconsContainer'>
         <Button buttonType='button' iconLeft='lum-icon-info-fill' />
         <Button buttonType='button' iconLeft='lum-icon-history' />
