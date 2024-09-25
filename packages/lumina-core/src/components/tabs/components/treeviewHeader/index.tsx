82b7245321ed5aea88a41bc3e34f2a11b0688bda
Adriana Alves <adrianaalves098@gmail.com>
2024-09-25 19:50:02 +0100

    import errors

--- packages/lumina-core/src/components/tabs/components/treeviewHeader/index.tsx
+++ packages/lumina-core/src/components/tabs/components/treeviewHeader/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/button'
 import cx from 'classnames'
-// import data from '../../../../../../../examples/vite-react/src/data/pageData'
+import data from '../../../../../../../examples/vite-react/src/data/pageData'
 import { useCallback } from 'react'
 
 export const TreeviewHeader = () => {
@@ -8,12 +8,12 @@ export const TreeviewHeader = () => {
     window.open(window.location.href.split('/').slice(0, -1).join('/'), '_blank')
   }, [])
 
-  // const friendlyName = data.page1.friendlyName
+  const friendlyName = data.page1.friendlyName
 
   return (
     <div className='treeviewHeaderContainer'>
       <span className={cx('treeviewHeaderIcon', 'lum-icon-page')}></span>
-      <p className='treeviewTitle'>FriendlyName</p>
+      <p className='treeviewTitle'>{friendlyName}</p>
       {/* <p className='treeviewSubTitle'>Home Page for Lumina PageBuilder</p> */}
       <div className='treeviewNavIconsContainer'>
         <Button buttonType='button' iconLeft='lum-icon-info-fill' />
